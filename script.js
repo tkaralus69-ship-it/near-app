edbe479011e93060fd57d2348749d63503c4c304
tkaralus69-ship-it <tkaralus69@gmail.com>
2025-12-30 21:29:24 +1030

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  message.innerText = "Loading...";
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      message.innerText = "Welcome to Near";
+    })
+    .catch(error => {
+      if (error.code === "auth/user-not-found") {
+        auth.createUserWithEmailAndPassword(email, password)
+          .then(() => {
+            message.innerText = "Welcome to Near";
+          })
+          .catch(err => {
+            message.innerText = err.message;
+          });
+      } else {
+        message.innerText = error.message;
+      }
+    });
+}
