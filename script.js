8b58d2081f419ae0380104845f3d6bd61ca91341
tkaralus69-ship-it <tkaralus69@gmail.com>
2025-12-30 23:36:57 +1030

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyA2ApGkST41s9U53GQIatv4FL8aCPVzeAM",
   authDomain: "near-c7681.firebaseapp.com",
@@ -15,23 +14,19 @@ const firebaseConfig = {
   appId: "1:316318833624:web:480beb2c1909e23d1cf0ad"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Get elements
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginBtn = document.getElementById("login");
 const signupBtn = document.getElementById("signup");
 
-// Sign up
 signupBtn.onclick = async () => {
   await createUserWithEmailAndPassword(auth, email.value, password.value);
   window.location.href = "dashboard.html";
 };
 
-// Log in
 loginBtn.onclick = async () => {
   await signInWithEmailAndPassword(auth, email.value, password.value);
   window.location.href = "dashboard.html";
