533a8497bfe87c0b06479570c8d037328641334f
tkaralus69-ship-it <tkaralus69@gmail.com>
2025-12-30 17:10:53 +1030

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("signupBtn").addEventListener("click", () => {
+  alert("Sign Up clicked");
+});
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  alert("Log In clicked");
+});
