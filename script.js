2a9aa2688febc5fbc3d910f9214547f7d1bf94ce
tkaralus69-ship-it <tkaralus69@gmail.com>
2025-12-31 12:34:27 +1030

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("✅ script.js is loaded!");
+
+const signupBtn = document.getElementById("signupBtn");
+const loginBtn = document.getElementById("loginBtn");
+
+signupBtn.onclick = () => {
+  alert("Sign Up button works");
+};
+
+loginBtn.onclick = () => {
+  alert("Log In button works");
+};
