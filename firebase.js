db7d48418b8842d2674fb984c23dbb58575ad295
tkaralus69-ship-it <tkaralus69@gmail.com>
2026-02-08 20:14:56 +1030

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js (ES module)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2ApGkST41s9U53GQIatv4FL8aCPVzeAM",
+  authDomain: "near-c7681.firebaseapp.com",
+  projectId: "near-c7681",
+  storageBucket: "near-c7681.firebasestorage.app",
+  messagingSenderId: "316318833624",
+  appId: "1:316318833624:web:480beb2c1909e23d1cf0ad",
+  measurementId: "G-98XYEKXLLT"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
